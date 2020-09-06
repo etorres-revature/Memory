1adf4b4924b09bd1c92f1f81eacd0b2fc3f18b14
Eric D. Torres <etorresnotary@gmail.com>
2020-09-06 16:47:18 -0500

    starting js

---
+++ app.js
@@ -0,0 +1,13 @@
+//adding DOM selectors to attach to HTML
+const cardsContainerEl = document.querySelector("#cards-container");
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const currentEl = document.querySelector("#current");
+const showBtn = document.querySelector("#show-btn");
+const hideBtn = document.querySelector("#hide-btn");
+const questionEl = document.querySelector("#question");
+const answerEl = document.querySelector("#answer");
+const addCardBtn = document.querySelector("#add-card-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const addContainerEl = document.querySelector("#add-card-container");
+
